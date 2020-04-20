b0e2694321c0c3a1110db79cf85f97fc5a8b2e44
GeraldRyan <gerald.ryan40@yahoo.com>
2020-04-20 17:25:04 -0500

    second test passes which includdes a rerendering of one component

--- src/components/Episodes.test.js
+++ src/components/Episodes.test.js
@@ -2,6 +2,21 @@ import React from 'react'
 import {render} from '@testing-library/react'
 import Episodes from './Episodes'
 
+const episodeTestData = [
+  1,
+  2,
+  3
+]
+
+
 test("Does the component Render?", ()=>{
   render(<Episodes episodes={[]}></Episodes>)
+})
+
+test("Check that episodes re-renders upon initialization", ()=>{
+  
+  const { rerender } = render(<Episodes episodes={[]}/>)
+  
+  rerender(<Episodes episodes={episodeTestData} />)
+
 })
